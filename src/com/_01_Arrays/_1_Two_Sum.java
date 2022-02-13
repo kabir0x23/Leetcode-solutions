c581c01e43c27db821f51b9cdcb8f6395a06cdf2
kabir0x17 <kabirj2310@gmail.com>
2022-02-13 18:24:17 +0530

    Create _1_Two_Sum.java

---
+++ src/com/_01_Arrays/_1_Two_Sum.java
@@ -0,0 +1,13 @@
+package com._01_Arrays;
+
+public class _1_Two_Sum {
+    public static void main(String[] args) {
+
+    }
+    static int[] twoSum(int[] nums, int target) {
+        int[] result = new int[nums.length];
+        return result;
+    }
+
+
+}
