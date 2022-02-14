9a871e00922ae15d346d8b9d48232f005067f1f4
kabir0x17 <kabirj2310@gmail.com>
2022-02-14 14:16:52 +0530

    Update _1_Two_Sum.java

--- src/com/_01_Arrays/_1_Two_Sum.java
+++ src/com/_01_Arrays/_1_Two_Sum.java
@@ -1,12 +1,25 @@
 package com._01_Arrays;
 
+import java.util.Arrays;
+
 public class _1_Two_Sum {
     public static void main(String[] args) {
+        int[] nums = {3, 2, 4};
+        int target = 6;
 
+        System.out.println(Arrays.toString(twoSum(nums, target)));
     }
+
     static int[] twoSum(int[] nums, int target) {
-        int[] result = new int[nums.length];
-        return result;
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    nums = new int[]{i, j};
+                }
+            }
+        }
+        return nums;
     }
 
 
