32b5c498d6ea937d063c35061e4cc1713d61196e
kabir0x17 <kabirj2310@gmail.com>
2022-02-14 14:16:56 +0530

    Create _1480_Running_Sum_of_1d_Array.java

---
+++ src/com/_01_Arrays/_1480_Running_Sum_of_1d_Array.java
@@ -0,0 +1,21 @@
+package com._01_Arrays;
+
+import java.util.Arrays;
+
+public class _1480_Running_Sum_of_1d_Array {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4};
+        System.out.println(Arrays.toString(runningSum(nums)));
+
+    }
+
+    static int[] runningSum(int[] nums) {
+        int[] runningSum = new int[nums.length];
+        runningSum[0] = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            runningSum[i] = runningSum[i - 1] + nums[i];
+        }
+        return runningSum;
+    }
+}
