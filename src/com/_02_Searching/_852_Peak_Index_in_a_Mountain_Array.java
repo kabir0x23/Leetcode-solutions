2714b76a67480279abd5a364e2ffc36ac6464d24
kabir0x17 <kabirj2310@gmail.com>
2022-02-21 20:10:24 +0530

    Create _852_Peak_Index_in_a_Mountain_Array.java

---
+++ src/com/_02_Searching/_852_Peak_Index_in_a_Mountain_Array.java
@@ -0,0 +1,24 @@
+package com._02_Searching;
+
+public class _852_Peak_Index_in_a_Mountain_Array {
+    public static void main(String[] args) {
+        int[] arr = {0, 1, 0};
+        System.out.println(peakIndexInMountainArray(arr));
+    }
+
+    static int peakIndexInMountainArray(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid] > arr[mid + 1]) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return start;
+    }
+}
