027508a996ce69b59dcc8052fc4b015f89072395
kabir0x17 <kabirj2310@gmail.com>
2022-03-01 23:05:41 +0530

    Update _442_Find_All_Duplicates_in_an_Array.java

--- src/com/_03_Sorting/_442_Find_All_Duplicates_in_an_Array.java
+++ src/com/_03_Sorting/_442_Find_All_Duplicates_in_an_Array.java
@@ -11,18 +11,18 @@ public class _442_Find_All_Duplicates_in_an_Array {
 
     static List<Integer> findDuplicates(int[] arr) {
         int i = 0;
-
         while (i < arr.length) {
             int correct = arr[i] - 1;
             if (arr[i] != arr[correct]) {
-                swap(arr, i , correct);
+                swap(arr, i, correct);
             } else {
                 i++;
             }
         }
+
         List<Integer> ans = new ArrayList<>();
         for (int index = 0; index < arr.length; index++) {
-            if (arr[index] != index+1) {
+            if (arr[index] != index + 1) {
                 ans.add(arr[index]);
             }
         }
@@ -35,4 +35,5 @@ public class _442_Find_All_Duplicates_in_an_Array {
         arr[first] = arr[second];
         arr[second] = temp;
     }
+
 }
