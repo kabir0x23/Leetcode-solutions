d4924dc8f9b6d2d3f57b4c093afac524afd5d6db
kabir0x17 <kabirj2310@gmail.com>
2022-03-06 18:56:56 +0530

    Update _1528_Shuffle_String.java

--- src/com/_04_Strings/_1528_Shuffle_String.java
+++ src/com/_04_Strings/_1528_Shuffle_String.java
@@ -4,17 +4,17 @@ public class _1528_Shuffle_String {
     public static void main(String[] args) {
         String s = "codeleet";
         int[] indices = {4, 5, 6, 7, 0, 2, 1, 3};
-        System.out.println(restoreString(s, indices));
+//        System.out.println(restoreString(s, indices));
 
     }
-    static String restoreString(String s, int[] indices) {
-
-        String[] values = {""};
-        for (int i = 0; i < s.length(); i++) {
-            values[i] =  indices[i];
-        }
-
-    }
+//    static String restoreString(String s, int[] indices) {
+//
+//        String[] values = {""};
+//        for (int i = 0; i < s.length(); i++) {
+//            values[i] =  indices[i];
+//        }
+//
+//    }
 
 }
 
