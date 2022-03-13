8f6fe4f3072d312c01142d9fc8767b96e08094fc
kabir0x17 <kabirj2310@gmail.com>
2022-03-13 21:40:59 +0530

    Update _1_Two_Sum.java

--- src/com/_01_Arrays/_1_Two_Sum.java
+++ src/com/_01_Arrays/_1_Two_Sum.java
@@ -13,7 +13,7 @@ public class _1_Two_Sum {
     static int[] twoSum(int[] nums, int target) {
 
         for (int i = 0; i < nums.length; i++) {
-            for (int j = 0; j < nums.length; j++) {
+            for (int j = 0; j < i; j++) {
                 if (nums[i] + nums[j] == target) {
                     return new int[]{i, j};
                 }
