969c34b1295e134c05a63cf4584abf30d5d4d160
kabir0x17 <kabirj2310@gmail.com>
2022-03-28 10:27:50 +0530

    Update _1929_Concatenation_of_Array.java

--- src/com/_01_Arrays/_1929_Concatenation_of_Array.java
+++ src/com/_01_Arrays/_1929_Concatenation_of_Array.java
@@ -10,11 +10,11 @@ public class _1929_Concatenation_of_Array {
 
     static int[] getConcatenation(int[] nums) {
         int n = nums.length;
-        int[] ans = new int[2 * n];
+        int[] ans = new int[nums.length * 2];
 
         for (int i = 0; i < nums.length; i++) {
-            ans[i] = nums[i];
-            ans[i + n] = nums[i];
+                ans[i] = nums[i];
+                ans[i + n] = nums[i];
         }
         return ans;
     }
