4d4233fa512ac20dcbaeaccba444896ba39c72b4
kabir0x17 <kabirj2310@gmail.com>
2022-03-29 11:24:46 +0530

    Update _1431_Kids_With_the_Greatest_Number_of_Candies.java

--- src/com/_01_Arrays/_1431_Kids_With_the_Greatest_Number_of_Candies.java
+++ src/com/_01_Arrays/_1431_Kids_With_the_Greatest_Number_of_Candies.java
@@ -4,21 +4,23 @@ import java.util.*;
 
 public class _1431_Kids_With_the_Greatest_Number_of_Candies {
     public static void main(String[] args) {
-        int[] candies = {2, 3, 5, 3};
+        int[] candies = {2, 3, 5, 1, 3};
         int extraCandies = 3;
         System.out.println(kidsWithCandies(candies, extraCandies));
     }
 
     static List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
-        List<Boolean> result = new ArrayList<>();
+        List<Boolean> result = new ArrayList<Boolean>();
         int max = -1;
-        for (int i : candies){
+        for (int i : candies) {
             max = Math.max(max, i);
         }
+
         for (int candy : candies) {
             result.add((candy + extraCandies) >= max);
         }
         return result;
     }
+
 
 }
