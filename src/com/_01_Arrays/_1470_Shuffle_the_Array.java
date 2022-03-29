806ab6df0803dc2529d502a34335abcef2f36048
kabir0x17 <kabirj2310@gmail.com>
2022-03-29 11:24:49 +0530

    Update _1470_Shuffle_the_Array.java

--- src/com/_01_Arrays/_1470_Shuffle_the_Array.java
+++ src/com/_01_Arrays/_1470_Shuffle_the_Array.java
@@ -5,14 +5,14 @@ import java.util.Arrays;
 public class _1470_Shuffle_the_Array {
     public static void main(String[] args) {
         int[] nums = {2, 5, 1, 3, 4, 7};
-        System.out.println(Arrays.toString(shuffle(nums, 3)));
+        int n = 3;
+        System.out.println(Arrays.toString(shuffle(nums, n)));
     }
 
     static int[] shuffle(int[] nums, int n) {
-        int[] ans = new int[2*n];
-
+        int[] ans = new int[2 * n];
         int j = 0;
-        for (int i = 0; i < ans.length; i += 2) {
+        for (int i = 0; i < ans.length; i +=2) {
             ans[i] = nums[j];
             ans[i + 1] = nums[j + n];
             j++;
