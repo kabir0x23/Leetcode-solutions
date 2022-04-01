74cd45708996aedb5f27f4942b1da160d68b52a3
kabir0x17 <kabirj2310@gmail.com>
2022-04-01 08:41:09 +0530

    Update _1365_How_Many_Numbers_Are_Smaller_Than_the_Current_Number.java

--- src/com/_01_Arrays/_1365_How_Many_Numbers_Are_Smaller_Than_the_Current_Number.java
+++ src/com/_01_Arrays/_1365_How_Many_Numbers_Are_Smaller_Than_the_Current_Number.java
@@ -11,12 +11,14 @@ public class _1365_How_Many_Numbers_Are_Smaller_Than_the_Current_Number {
     static int[] smallerNumbersThanCurrent(int[] nums) {
         int[] smallerNumber = new int[nums.length];
 
-        for (int i = 0; i < smallerNumber.length; i++) {
-            for (int j = 0; j < smallerNumber.length; j++) {
-                if(j!=i & nums[j] < nums[i])
-                smallerNumber[i]++;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (j != i & nums[j] < nums[i]) {
+                    smallerNumber[i]++;
+                }
             }
         }
         return smallerNumber;
     }
+
 }
