31d7357de049c4066ce0378b466592998e697906
kabir0x17 <kabirj2310@gmail.com>
2022-04-03 09:53:23 +0530

    Update _1773_Count_Items_Matching_a_Rule.java

--- src/com/_01_Arrays/_1773_Count_Items_Matching_a_Rule.java
+++ src/com/_01_Arrays/_1773_Count_Items_Matching_a_Rule.java
@@ -1,22 +1,22 @@
-package com._01_Arrays;
-
-import java.util.*;
-
-public class _1773_Count_Items_Matching_a_Rule {
-    public static void main(String[] args) {
-        ArrayList<ArrayList<String>> items = new ArrayList<ArrayList<String>>();
-        String[][] items3 = {{"phone", "blue", "pixel"}, {"computer", "silver", "lenovo"}, {"phone", "gold", "iphone"}};
-        String ruleKey = "color";
-        String ruleValue = "silver";
-
-        for (int i = 0; i <= 3; i++) {
-
-        }
-
-        countMatches(items, ruleKey, ruleValue);
-    }
-
-    static int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
-
-    }
-}
+//package com._01_Arrays;
+//
+//import java.util.*;
+//
+//public class _1773_Count_Items_Matching_a_Rule {
+//    public static void main(String[] args) {
+//        List<List<String>> items = new List<>();
+//        String[][] items3 = {{"phone", "blue", "pixel"}, {"computer", "silver", "lenovo"}, {"phone", "gold", "iphone"}};
+//        String ruleKey = "color";
+//        String ruleValue = "silver";
+//
+//        for (int i = 0; i <= 3; i++) {
+//
+//        }
+//
+//        countMatches(items, ruleKey, ruleValue);
+//    }
+//
+//    static int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
+//
+//    }
+//}
