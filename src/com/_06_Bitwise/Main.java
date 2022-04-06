0cc2bd2750d1b9e90ece9c254a7ff668e7c737df
kabir0x17 <kabirj2310@gmail.com>
2022-04-06 19:36:05 +0530

    Bitwise

---
+++ src/com/_06_Bitwise/Main.java
@@ -0,0 +1,9 @@
+package com._06_Bitwise;
+
+public class Main {
+    public static void main(String[] args) {
+
+            System.out.println(10 << 2);
+
+        }
+    }
---
+++ src/com/_06_Bitwise/NoOfDigits.java
@@ -0,0 +1,12 @@
+package com._06_Bitwise;
+
+public class NoOfDigits {
+    public static void main(String[] args) {
+        int n = 34567;
+        int b = 2 ;
+
+
+        int ans = (int) (Math.log(n) / Math.log(b)) + 1;
+        System.out.println(ans);
+    }
+}
