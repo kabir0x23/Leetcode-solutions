7728f9e5eeaee07950290e8fb61d94ccd971bae9
kabir0x17 <kabirj2310@gmail.com>
2022-04-11 09:46:32 +0530

    Update Reverse.java

--- src/com/_05_Recursion/Easy/Reverse.java
+++ src/com/_05_Recursion/Easy/Reverse.java
@@ -1,22 +1,38 @@
 package com._05_Recursion.Easy;
 
 public class Reverse {
-
-
     static int sum = 0;
 
-    static void fun(int n) {
+    static void reverse(int n) {
         if (n == 0) {
             return;
         }
         int rem = n % 10;
         sum = sum * 10 + rem;
-        fun(n / 10);
+
+        reverse(n / 10);
+    }
+
+    static int rev2(int n) {
+        int digits = (int) (Math.log10(n)) + 1;
+        return helper(n, digits);
+    }
+
+    private static int helper(int n, int digits) {
+        if (n % 10 == n) {
+            return n;
+        }
+        int rem = n % 10;
+
+        return rem * (int) Math.pow(10, digits - 1) + helper(n / 10, digits - 1);
     }
 
     public static void main(String[] args) {
-        fun(1842);
+        int ans = 1234;
+        reverse(ans);
         System.out.println(sum);
+        System.out.println(rev2(1234));
 
     }
 }
+
