8450ba3c6f97a28e5aa23480ff69907af64baf9b
Kabir <44284877+kabir0x23@users.noreply.github.com>
2022-05-02 09:49:12 -0700

    Create _1732_Find_the_Highest_Altitude.java

---
+++ src/com/_01_Arrays/Easy/_1732_Find_the_Highest_Altitude.java
@@ -0,0 +1,13 @@
+package com._01_Arrays.Easy;
+
+public class _1732_Find_the_Highest_Altitude {
+    public static void main(String[] args) {
+        int[] gain = {-5, 1, 5, 0, -7};
+        System.out.println(largestAltitude(gain));
+    }
+
+    static int largestAltitude(int[] gain) {
+
+        return 0;
+    }
+}
