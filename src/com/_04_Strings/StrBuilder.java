df2ffe93878bb5dc187657389edf6d504743d5a4
Kabir <44284877+kabir0x23@users.noreply.github.com>
2022-05-20 23:13:17 +0530

    Update StrBuilder.java

--- src/com/_04_Strings/StrBuilder.java
+++ src/com/_04_Strings/StrBuilder.java
@@ -2,8 +2,6 @@ package com._04_Strings;
 
 public class StrBuilder {
     public static void main(String[] args) {
-
-
         StringBuilder builder = new StringBuilder();
         for (int i = 0; i < 26; i++) {
             char ch = (char) ('a' + i);
